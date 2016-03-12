2d3153c4d5e942437719df122198ae30bc56f49f
MattTheMicrowave <mwlluis@hotmail.com>
2016-03-11 19:46:48 -0600

    got rid of first item
    got rid of first item

--- js/to-do-angular.js
+++ js/to-do-angular.js
@@ -1,15 +1,17 @@
 angular.module('ToDo', [])
 .controller('todoController', ['$scope', function ($scope) {
 
-    $scope.todos = [{'title': 'build', 'done':false}];
-
+    $scope.todos = [];
+//the list will be initiated as an empty array, if you want to have a starting item at initiation of page loading, you have to hard code it in the above array
     
     // ok so initializing the array as empty allows the app to start 'from             scratch', hence no initial text and no initial bulletpoint 
         
     
     $scope.addTodo = function () {
             
-    $scope.todos.push({'title': $scope.newtodo});
+    $scope.todos.push({'title': $scope.newtodo, 'done':false});
+//    the above code ensures that all of the new objects will be added on with a done property of false
+        
     $scope.newtodo = '';
     // the above line is what resets the input box to empty after submission
         };
@@ -25,4 +27,5 @@ angular.module('ToDo', [])
     });
     
     };
-    }])
+    
+        }])
